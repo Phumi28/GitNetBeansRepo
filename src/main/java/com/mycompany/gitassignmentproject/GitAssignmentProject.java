06112eb5afc61f2b86c0348b5fd2e9b36efd05fe
Administrator <administrator@localhost>
2026-05-01 13:48:38 +0200

    Initial commit - Hello NetBeans program

---
+++ src/main/java/com/mycompany/gitassignmentproject/GitAssignmentProject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitassignmentproject;
+
+/**
+ *
+ * @author Administrator
+ */
+public class GitAssignmentProject {
+
+ public static void main(String[] args) {
+        System.out.println("Hello NetBeans. Welcome to version control using Git");
+    }
+}
+    
