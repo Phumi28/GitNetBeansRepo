dfd6e03f6ae0bc91aa22030b68cf782d9eac697e
Administrator <administrator@localhost>
2026-05-01 14:22:23 +0200

    Initial commit - Hello NetBeans program

--- src/main/java/com/mycompany/gitassignmentproject/GitAssignmentProject.java
+++ src/main/java/com/mycompany/gitassignmentproject/GitAssignmentProject.java
@@ -11,7 +11,7 @@ package com.mycompany.gitassignmentproject;
 public class GitAssignmentProject {
 
  public static void main(String[] args) {
-        System.out.println("Hello NetBeans. Welcome to version control using Git");
+        System.out.println("Hello NetBeans. Welcome to version control using Git and add");
     }
 }
     
